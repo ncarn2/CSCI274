cf58c8058a74e6ec35f18c831144a821bf8d91f2
Nicholas Carnival <ncarnival2@yahoo.com>
2019-03-04 12:55:28 -0700

    finished UnixHistory.sh

--- bashStuff/UnixHistory.sh
+++ bashStuff/UnixHistory.sh
@@ -1,3 +1,7 @@
+#Nicholas Carnival 
+mkdir ResearchUnix && echo "1972" > ResearchUnix/Year
+cd ResearchUnix && mkdir BSD Commercial
+cd BSD && mkdir FreeBSD NextStep && echo "1977" > Year && echo "1993" > FreeBSD/Year
+cd NextStep && mkdir MacOSX && echo "1988" > Year && echo "2002" > MacOSX/Year
+cd ../../Commercial && echo "1984" > Year && mkdir Solaris && echo "1991" > Solaris/Year
 
-mkdir ResearchUnix
-cd ResearchUnix && mkdir BSD
