f54b9eb2c0b46aea9bc65e531d270caeaf90e74b
ncarn2 <carnivalnicholas2@gmail.com>
2019-03-20 13:51:30 -0600

    updated quoth.sh, it now outputs the correct thing only if the directory exists

--- bashStuff/Quoth/Quoth.sh
+++ bashStuff/Quoth/Quoth.sh
@@ -5,6 +5,9 @@ read input
 if [[ $input == "QUIT" ||  $input == "quit" ]]; then
     echo "we not doing it"
 else
-    echo "we doing it"
+    echo $input 
+    #if the directory exists, we should cat its quote
+    cat $input/quote.txt
+    #if the directory does not exist, exit status 7 
 fi
 
