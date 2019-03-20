6aefab213f1662bfe95e32e93116e4b46db524e0
ncarn2 <carnivalnicholas2@gmail.com>
2019-03-20 13:12:39 -0600

    deleted some old files and directories, moved unixhistory to where it belongs

--- bashStuff/UnixHistory/UnixHistory.sh
+++ bashStuff/UnixHistory/UnixHistory.sh
@@ -1,7 +1,19 @@
-#Nicholas Carnival 
-mkdir ResearchUnix && echo "1972" > ResearchUnix/Year
-cd ResearchUnix && mkdir BSD Commercial
-cd BSD && mkdir FreeBSD NextStep && echo "1977" > Year && echo "1993" > FreeBSD/Year
-cd NextStep && mkdir MacOSX && echo "1988" > Year && echo "2002" > MacOSX/Year
-cd ../../Commercial && echo "1984" > Year && mkdir Solaris && echo "1991" > Solaris/Year
+#!usr/bin/env bash   
 
+mkdir ResearchUnix
+cd ResearchUnix && mkdir BSD, Commercial
+cd BSD && mkdir FreeBSD, NextStep
+cd NextStep && mkdir MacOSX
+cd ../BSD && mkdir Solaris
+
+#recursively puts the year file into each dir
+echo 
+
+#Year data
+ResearchUnix = 1972
+Commercial = 1984
+BSD = 1977
+Solaris = 1991
+FreeBSD = 1993
+NextStep = 1988
+MacOSX = 2002
