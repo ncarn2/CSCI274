76600b00838c9a9800f7956f02ad79a44f8cc950
ncarn2 <carnivalnicholas2@gmail.com>
2019-03-21 22:48:13 -0600

    finished quoth.sh

--- bashStuff/Quoth/Quoth.sh
+++ bashStuff/Quoth/Quoth.sh
@@ -1,13 +1,22 @@
-#display current directory
-ls -d */
-echo "Type a directory name or QUIT: " 
-read input
-if [[ $input == "QUIT" ||  $input == "quit" ]]; then
-    echo "we not doing it"
-else
-    echo $input 
-    #if the directory exists, we should cat its quote
-    cat $input/quote.txt
-    #if the directory does not exist, exit status 7 
-fi
+#Author: Nicholas Carnival
+#Due: 03/22/19
 
+while [[ "$input" != "QUIT" && "$input" != "quit" ]]
+do
+    echo "Pick a quote-sayer from:"
+    #displays all directories in the current working directory
+    ls -1 */ | sed 's#/##'
+    echo "... or enter QUIT to exit the script. " 
+    read input
+    #if the directory exists, we should cat its quote
+    if [ -d $input ]; then 
+        echo  "$input said..."
+        cat $input/quote.txt
+        echo
+    elif [[ $input = "quit" || $input = "QUIT" ]]; then
+        exit 0
+    elif [ ! -d "$input" ]; then
+        exit 7
+    fi
+    #if the directory does not exist, exit status 7 
+done
