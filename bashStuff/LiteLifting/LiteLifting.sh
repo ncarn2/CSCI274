5b11a52e94315610eb90cfc2f88c0780159cf46e
ncarn2 <carnivalnicholas2@gmail.com>
2019-04-24 21:04:26 -0600

    updated LitLifting.sh and made a test file

--- bashStuff/LiteLifting/LiteLifting.sh
+++ bashStuff/LiteLifting/LiteLifting.sh
@@ -1,6 +1,10 @@
 #Nicholas Carnival
 #Lite Lifting
 
+numChars=$(wc -c < $1)
 
+echo 'This file has' "${numChars}" 'characters in it.'
 
-
+#some type of wc piped into something
+numCharsNoLine=$()
+echo 'This file has' "${numCharsNoLine}" 'characters not counting newline.'
