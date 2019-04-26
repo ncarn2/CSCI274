30f2297ec7928eca575317de061d6552b9980373
ncarn2 <carnivalnicholas2@gmail.com>
2019-04-26 16:10:28 -0600

    properly counts empty lines, total characters, and chars w/o newline

--- bashStuff/LiteLifting/LiteLifting.sh
+++ bashStuff/LiteLifting/LiteLifting.sh
@@ -5,16 +5,23 @@ numChars=$(wc -c < $1)
 
 echo 'This file has' "${numChars}" 'characters in it.'
 
+#number of total lines
+numTotalLines=$(wc -l < $1 )
+
 #some type of wc piped into something
-numCharsNoLine=$(wc -c < $1)
+numCharsNoLine=$( tr -d '\n' < $1 | wc -c)
+
+#the number of lines excluding empty lines
+numNonEmptyLines=$(sed '/^$/d' $1 | wc -l)
 
 echo 'This file has' "${numCharsNoLine}" 'characters not counting newline.'
 
-numEmptyLines=$(wc -c |tr -d '\n' < $1)
+#use sed to find all '\n' or use awk
+numEmptyLines=$(( numTotalLines - numNonEmptyLines ))
 echo 'This file has' "${numEmptyLines}" 'empty line.'
 
 numAlphaCharacters=$(wc -c < $1 )
 echo 'This file contains' "${numAlhpaCharacter}" 'alphanumberic characters.'
 
-numChars=$(wc -c < $1)
-echo 'The 4th word of the 3rd line of text is' "/${numChars}/"
+fourthWord=$(wc -c < $1)
+echo 'The 4th word of the 3rd line of text is' "/${fourthWord}/"
