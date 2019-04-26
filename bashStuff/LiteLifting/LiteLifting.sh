2c9bef294ad7bef9ed8f5a3a76bb99d9c0158607
ncarn2 <carnivalnicholas2@gmail.com>
2019-04-26 17:03:54 -0600

    fixed numAlphaCharacters

--- bashStuff/LiteLifting/LiteLifting.sh
+++ bashStuff/LiteLifting/LiteLifting.sh
@@ -1,28 +1,29 @@
 #Nicholas Carnival
 #Lite Lifting
 
+#number of characters in the file
 numChars=$(wc -c < $1)
-
 echo 'This file has' "${numChars}" 'characters in it.'
 
-#number of total lines
-numTotalLines=$(wc -l < $1 )
-
-#some type of wc piped into something
+#number of chars excluding newline
 numCharsNoLine=$( tr -d '\n' < $1 | wc -c)
-
-#the number of lines excluding empty lines
-numNonEmptyLines=$(sed '/^$/d' $1 | wc -l)
-
 echo 'This file has' "${numCharsNoLine}" 'characters not counting newline.'
 
-#use sed to find all '\n' or use awk
+#total number of lines
+numTotalLines=$(wc -l < $1 )
+#number of non empty lines
+numNonEmptyLines=$(sed '/^$/d' $1 | wc -l)
+#number of emty lines
 numEmptyLines=$(( numTotalLines - numNonEmptyLines ))
-echo 'This file has' "${numEmptyLines}" 'empty line.'
+echo 'This file has' "${numEmptyLines}" 'empty lines.'
 
-#numbers and letters
-numAlphaCharacters=$(sed 's/[^a-zA-Z0-9]//g' $1 | wc -c) 
+#alphanumeric characters
+numAlphaCharacters=$(sed 's/[^a-zA-Z0-9]//g' $1 | tr -d '\n' |  wc -c) 
 echo 'This file contains' "${numAlphaCharacters}" 'alphanumeric characters.'
 
+#fourth  word third line
 fourthWord=$(sed -n 3p $1 | awk '{print $4}')
 echo 'The 4th word of the 3rd line of text is' "/${fourthWord}/"
+
+#Capital Idea Logic
+
