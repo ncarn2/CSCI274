1e850002c7c2e086343676dc8f398c23c6c97293
ncarn2 <carnivalnicholas2@gmail.com>
2019-04-25 18:32:39 -0600

    update LiteLifting to have the proper templaye

--- bashStuff/LiteLifting/LiteLifting.sh
+++ bashStuff/LiteLifting/LiteLifting.sh
@@ -6,5 +6,15 @@ numChars=$(wc -c < $1)
 echo 'This file has' "${numChars}" 'characters in it.'
 
 #some type of wc piped into something
-numCharsNoLine=$()
+numCharsNoLine=$(wc -c < $1)
+
 echo 'This file has' "${numCharsNoLine}" 'characters not counting newline.'
+
+numEmptyLines=$(wc -c |tr -d '\n' < $1)
+echo 'This file has' "${numEmptyLines}" 'empty line.'
+
+numAlphaCharacters=$(wc -c < $1 )
+echo 'This file contains' "${numAlhpaCharacter}" 'alphanumberic characters.'
+
+numChars=$(wc -c < $1)
+echo 'The 4th word of the 3rd line of text is' "/${numChars}/"
---
+++ bashStuff/LiteLifting/infiniteloop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+i=0
+while [ $i -lt $1 ]
+do
+echo 'word'
+i=$[$i+1]
+done
+
