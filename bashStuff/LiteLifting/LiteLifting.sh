08c7111beba2d4ddd3dea2407daa60f439c876ad
ncarn2 <carnivalnicholas2@gmail.com>
2019-04-26 19:18:38 -0600

    shortened everything and fixed off by one errors

--- bashStuff/LiteLifting/LiteLifting.sh
+++ bashStuff/LiteLifting/LiteLifting.sh
@@ -2,28 +2,19 @@
 #Lite Lifting
 
 #number of characters in the file
-numChars=$(wc -c < $1)
-echo 'This file has' "${numChars}" 'characters in it.'
+echo 'This file has' "$(( $( wc -c < $1 ) - 1 ))" 'characters in it.'
 
 #number of chars excluding newline
-numCharsNoLine=$( tr -d '\n' < $1 | wc -c)
-echo 'This file has' "${numCharsNoLine}" 'characters not counting newline.'
+echo 'This file has' "$(( $(tr -d '\n' < $1 | wc -c) - 1 ))" 'characters not counting newline.'
 
-#total number of lines
-numTotalLines=$(wc -l < $1 )
-#number of non empty lines
-numNonEmptyLines=$(sed '/^$/d' $1 | wc -l)
 #number of emty lines
-numEmptyLines=$(( numTotalLines - numNonEmptyLines ))
-echo 'This file has' "${numEmptyLines}" 'empty lines.'
+echo 'This file has' "$(( $(sed '/^$/d' $1 | wc -l) - (wc -l < $1) - 1 ))" 'empty lines.'
 
 #alphanumeric characters
-numAlphaCharacters=$(sed 's/[^a-zA-Z0-9]//g' $1 | tr -d '\n' |  wc -c) 
-echo 'This file contains' "${numAlphaCharacters}" 'alphanumeric characters.'
+echo 'This file contains' "$(( $(sed 's/[^a-zA-Z0-9]//g' $1 | wc -c) - 1 ))" 'alphanumeric characters.'
 
 #fourth  word third line
-fourthWord=$(sed -n 3p $1 | awk '{print $4}')
-echo 'The 4th word of the 3rd line of text is' "/${fourthWord}/"
+echo 'The 4th word of the 3rd line of text is' "/$( sed -n 3p $1 | awk '{print $4}' )/"
 
 #Capital Idea Logic
 
