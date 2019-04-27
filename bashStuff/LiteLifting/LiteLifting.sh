a31d5ce389c58432c75052d36a38e4aea8cbdc08
ncarn2 <carnivalnicholas2@gmail.com>
2019-04-26 21:12:59 -0600

    finished LiteLifting

--- bashStuff/LiteLifting/LiteLifting.sh
+++ bashStuff/LiteLifting/LiteLifting.sh
@@ -1,6 +1,8 @@
 #Nicholas Carnival
 #Lite Lifting
 
+#all of these require - 1 because the echo adds a '\n' 
+
 #number of characters in the file
 echo 'This file has' "$(( $( wc -c < $1 ) - 1 ))" 'characters in it.'
 
@@ -8,13 +10,16 @@ echo 'This file has' "$(( $( wc -c < $1 ) - 1 ))" 'characters in it.'
 echo 'This file has' "$(( $(tr -d '\n' < $1 | wc -c) - 1 ))" 'characters not counting newline.'
 
 #number of emty lines
-echo 'This file has' "$(( $(sed '/^$/d' $1 | wc -l) - (wc -l < $1) - 1 ))" 'empty lines.'
+echo 'This file has' "$(( $(wc -l < $1) - $(sed '/^$/d' $1 | wc -l) ))" 'empty lines.'
 
 #alphanumeric characters
-echo 'This file contains' "$(( $(sed 's/[^a-zA-Z0-9]//g' $1 | wc -c) - 1 ))" 'alphanumeric characters.'
-
+echo 'This file contains' "$( cat $1 | tr -cd [:alnum:] | wc -c )" 'alphanumeric characters.'
 #fourth  word third line
 echo 'The 4th word of the 3rd line of text is' "/$( sed -n 3p $1 | awk '{print $4}' )/"
 
 #Capital Idea Logic
+grep -q 'CapitalIdea' <<< "$( tr '\n' ' ' < $1 | tr -d ' ' )" 
+if [ $? -eq 0 ];then
+    cat $1 | tr '[:upper:]' '[:lower:]' > $1.lc
+fi
 
--- bashStuff/LiteLifting/infiniteloop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-i=0
-while [ $i -lt $1 ]
-do
-echo 'word'
-i=$[$i+1]
-done
-
